3d59a05477c5b669dbef78994d8d089393a5f2e7
EderDSouza <velasquessouza@gmail.com>
2022-09-09 09:20:59 -0300

    botão start em funcionamento

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-window.onload = function(){
 
-var segundos= 00;
-var tempo = 00;
+var segundos = 00;
 
 
+function start(){
+    setInterval(contador,100)
 
-botao_iniciar.onclick
+};
+
+function pausar(){
+    console.log("iniciou")
+
+};
+
+function reset(){
+    console.log("iniciou")
+
+};
+
+function contador(){
+    
+    segundos ++
+    document.getElementById("num_segundos").innerText= segundos
+    
+};
 
 
-}
+
+
